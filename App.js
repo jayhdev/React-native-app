c2e78f1f424bd931d92ab096a8d7d129ed0a08da
Smart Coder <smartcoder415@gmail.com>
2019-10-28 13:48:37 -0400

    Update root project tree

--- App.js
+++ App.js
@@ -1,20 +1,4 @@
-import React from 'react';
-import { AppRegistory } from 'react-native';
-import { StyleSheet, Text, View } from 'react-native';
+import { AppRegistry } from 'react-native';
+import MainApp from './Src/App'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Event Mobile App</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+AppRegistry.registerComponent('EventApp', () => MainApp)
