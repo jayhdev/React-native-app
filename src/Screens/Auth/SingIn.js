bded05f081586a13a61a672c4f310a526fd2bb33
Smart Coder <smartcoder415@gmail.com>
2019-10-31 16:01:04 -0400

    Basic Signin page

---
+++ src/Screens/Auth/SingIn.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import { Text, TextInput, KeyboardAvoidingView, ScrollView, Button } from 'react-native';
+import { connect } from 'react-redux';
+
+import styles from './AuthStyles';
+
+class SignIn extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      error: '' || (this.props.navigation.state.params && this.props.navigation.state.params.error)
+    };
+    this.handleChangeEmail = this.handleChangeEmail.bind(this);
+    this.handleChangePassword = this.handleChangePassword.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChangeEmail(value) {
+    this.setState({email: value});
+  }
+
+  handleChangePassword(value) {
+    this.setState({password: value});
+  }
+
+  handleSubmit() {
+    const email = this.state.email;
+    const password = this.state.password;
+
+    console.log("Login is submitted")
+    // this.props.login({
+    //   email,
+    //   password
+    // }, this.props.navigation);
+    // clear the state after login for security
+    this.setState({
+      email: '',
+      password: '',
+      error: ''
+    });
+  }
+
+  render() {
+   return (
+    <KeyboardAvoidingView behavior="position" style={styles.container}>
+      <ScrollView>
+        <Text style={styles.error}>{this.state.error}</Text>
+        <Text style={styles.textLabel}>Email</Text>
+        <TextInput
+          style={styles.textInput}
+          autoCapitalize="none"
+          autoCorrect={false}
+          maxLength={15}
+          placeholder="EMAIL"
+          placeholderTextColor="tomato"
+          value={this.state.email}
+          onChangeText={(email) => this.handleChangeEmail(email)}
+        />
+        <Text style={styles.textLabel}>Password</Text>
+        <TextInput
+          style={styles.textInput}
+          secureTextEntry={true}
+          autoCapitalize="none"
+          autoCorrect={false}
+          maxLength={15}
+          placeholder="PASSWORD"
+          placeholderTextColor="tomato"
+          value={this.state.password}
+          onChangeText={(password) => this.handleChangePassword(password)}
+        />
+        <Button
+          buttonStyle={styles.button}
+          title="Login"
+          onPress={this.handleSubmit}
+        />
+        <Button
+          buttonStyle={styles.button}
+          title="Sign Up"
+          onPress={() => {
+            this.props.navigation.navigate('SignUp');
+            this.setState({
+              email: '',
+              password: '',
+              error: ''
+            });
+          }}
+        />
+      </ScrollView>
+    </KeyboardAvoidingView>
+  );
+ }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  // login: (credentials, navigation) => dispatch(login(credentials, navigation))
+});
+
+export default connect(null, mapDispatchToProps)(SignIn);
