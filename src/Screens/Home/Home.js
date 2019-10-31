abbb56b600980a393fc539dd5d869a71f553404d
Smart Coder <smartcoder415@gmail.com>
2019-10-31 16:01:20 -0400

    Bare Home Screen

---
+++ src/Screens/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import styles from './HomeStyles';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Event Mobile App</Text>
+    </View>
+  );
+}
