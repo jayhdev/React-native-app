b95ea358a49fc8ecaea24854f4cfbbc0553a6e4e
Passion Coder <passioncoder@outlook.com>
2019-11-06 18:41:27 -0500

    React Native Element theme config

---
+++ src/Config/color.js
@@ -0,0 +1,10 @@
+const colors = {
+  primary: '#f5f5f3',
+  secondary: '#8430cb',
+  gray: '#aeaeae',
+  background: '#fcfcfa',
+  backgroundDark: '#f5f5f3',
+  black: '#060606',
+};
+
+export default colors;
