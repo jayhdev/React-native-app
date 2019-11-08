e64605108e2800f90d09771f7c8850a0b9e9192b
Smart Coder <smartcoder415@gmail.com>
2019-11-08 12:55:33 -0500

    Theme cutomization for button

--- src/Config/theme.js
+++ src/Config/theme.js
@@ -3,6 +3,22 @@ import colors from './color'
 const theme = {
   colors: {
     ...colors
+  },
+  Button: {
+    titleStyle: {
+      color: 'white',
+      fontSize: 18
+    },
+    buttonStyle: {
+      backgroundColor: colors.secondary,
+      width: '100%',
+      height: 50,
+      borderRadius: 10,
+      alignSelf: 'center',
+      marginTop: 10
+    }
+  },
+  Input: {
   }
 };
 
