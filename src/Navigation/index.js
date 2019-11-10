f58e81607bac4657423f49c3bc1b3ecc0d41c54c
Smart Coder <smartcoder415@gmail.com>
2019-11-09 19:33:01 -0500

    Navigation between footer screens

--- src/Navigation/index.js
+++ src/Navigation/index.js
@@ -4,6 +4,11 @@ import {
 } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import HomeScreen from '../Screens/Home';
+import Checklist from '../Screens/Checklist';
+import Finances from '../Screens/Finances';
+import Guests from '../Screens/Guests';
+import Overview from '../Screens/Overview';
+import TimeLine from '../Screens/TimeLine';
 import SignInScreen from '../Screens/Auth/SingIn';
 import SignUpScreen from '../Screens/Auth/SingUp';
 
@@ -19,7 +24,12 @@ const AuthStack = createStackNavigator(
 
 const AppStack = createStackNavigator(
   {
-    HomeScreen: { screen: HomeScreen }
+    HomeScreen: { screen: HomeScreen },
+    Checklist: { screen: Checklist },
+    Finances: { screen: Finances },
+    Guests: { screen: Guests },
+    Overview: { screen: Overview },
+    TimeLine: { screen: TimeLine }
   },
   {
     // Default config for all screens
