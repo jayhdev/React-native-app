d1fd13253dc9e06cf196de7c9db9c1a5620203ce
Smart Coder <smartcoder415@gmail.com>
2019-11-11 09:38:44 -0500

    ListItem common component

--- src/Screens/Checklist/styles.js
+++ src/Screens/Checklist/styles.js
@@ -1,11 +1,12 @@
 import { StyleSheet } from 'react-native';
+import colors from '../../Config/color';
 
 export default StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: colors.backgroundDark,
+    margin: 5,
+    padding: 10,
+    borderRadius: 5
   },
 });
   
---
+++ src/styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+import colors from './Config/color'
+
+export default StyleSheet.create({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    flex: 1,
+    backgroundColor: 'red',
+    justifyContent: 'center',
+  },
+});
+  
