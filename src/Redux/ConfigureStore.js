901972828c658516a21104d492bd21f9f1b8888b
Passion Coder <passioncoder@outlook.com>
2019-11-11 09:39:21 -0500

    Configure Store merging

--- src/Redux/ConfigureStore.js
+++ src/Redux/ConfigureStore.js
@@ -1,7 +1,35 @@
-import createStore from './CreateStore'
+import { createStore, applyMiddleware, compose } from 'redux'
+import createSagaMiddleware from 'redux-saga'
+import { createLogger } from 'redux-logger'
 import rootReducer from './Reducers'
 import rootSaga from './Sagas'
 
 export default () => {
-  return createStore(rootReducer, rootSaga)
+  /* ------------- Redux Configuration ------------- */
+
+  const middleware = []
+  const enhancers = []
+
+  /* ------------- Saga Middleware ------------- */
+
+  const sagaMiddleware = createSagaMiddleware()
+  middleware.push(sagaMiddleware)
+
+  /* ------------ Logger Middleware ------------- */
+  // if (DebugSetting.reduxLogging) {
+  middleware.push(createLogger())
+  // }
+
+  /* ------------- Assemble Middleware ------------- */
+
+  enhancers.push(applyMiddleware(...middleware))
+
+  /* ------------- AutoRehydrate Enhancer ------------- */
+
+  // in dev mode, we'll create the store through Reactotron
+  const store = createStore(rootReducer, compose(...enhancers))
+
+  sagaMiddleware.run(rootSaga)
+
+  return store
 }
