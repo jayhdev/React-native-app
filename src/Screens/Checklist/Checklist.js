4a798c88d016f21a12009503b985d84e44579498
Passion Coder <passioncoder@outlook.com>
2019-11-11 09:40:39 -0500

    Checklist Basic view with temp items

--- src/Screens/Checklist/Checklist.js
+++ src/Screens/Checklist/Checklist.js
@@ -2,11 +2,34 @@ import React from 'react';
 import { Text, View } from 'react-native';
 import styles from './styles';
 import Footer from '../../Layout/Footer'
+import { ListItem } from 'react-native-elements'
+// import { ListItem } from '../../Components'
 
 export default function Checklist({navigation}) {
+  const checklists = [
+    {
+      id: '1',
+      title: '13 months'
+    },
+    {
+      id: '2',
+      title: '12 months'
+    },
+  ]
+
   return (
-    <View style={styles.container}>
-      <Text>Checklist Page</Text>
+    <View>
+      {
+        checklists.map((checklist) => (
+          <ListItem 
+            key={checklist.id} 
+            title={checklist.title}
+            subtitle={checklist.title}
+            containerStyle={styles.container}
+            chevron
+          />
+        ))
+      }
       <Footer navigation={navigation}/>
     </View>
   );
