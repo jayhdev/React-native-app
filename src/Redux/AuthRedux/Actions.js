b8349cad1e0e8885a59c0be0f861a4d480409777
Passion Coder <passioncoder@outlook.com>
2019-11-12 10:53:13 -0500

    Authenticating user after loggin in

--- src/Redux/AuthRedux/Actions.js
+++ src/Redux/AuthRedux/Actions.js
@@ -7,3 +7,18 @@ export function signupRequest(email, password) {
     password
   }
 }
+
+export function loginRequest(email, password) {
+  return {
+    type: CONSTANTS.LOGIN_REQUEST,
+    email,
+    password
+  }
+}
+
+export function loginSuccess(token) {
+  return {
+    type: CONSTANTS.LOGIN_SUCCESS,
+    payload: token
+  }
+}
--- src/Redux/AuthRedux/Sagas.js
+++ src/Redux/AuthRedux/Sagas.js
@@ -1,13 +1,35 @@
 import { put, takeLatest, call } from 'redux-saga/effects';
 import * as CONSTANTS from './Constants'
-import request from '../../Services/api'
+import apiService from '../../Services/api'
+import navigationService from '../../Services/navigation'
+import { loginSuccess } from './Actions'
+// import notify from '../../Services/notify'
 
 function* signupRequest(action) {
   const {email, password} = action
 
-  yield call(request, '/signup', {email, password}, 'post', false)
+  yield call(apiService, '/signup', {email, password}, 'post', false)
+  navigationService.navigate('Login')
+}
+
+function* loginRequest(action) {
+  try {
+    const {email, password} = action
+  
+    const {data} = yield call(apiService, '/login', {email, password}, 'post', false)
+  
+    if (data.success) {
+      yield put(loginSuccess(data.data.token))
+      navigationService.navigate('HomeScreen')
+    } else {
+      throw new Error(data.message)
+    }
+  } catch(e) {
+    console.log("Error in login", e)
+  }
 }
 
 export default function* authSaga() {
   yield takeLatest(CONSTANTS.SIGNUP_REQUEST, signupRequest);
+  yield takeLatest(CONSTANTS.LOGIN_REQUEST, loginRequest);
 }
