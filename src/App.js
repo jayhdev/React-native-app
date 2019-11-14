0fe37bdb9c8dc6fa9de50b37a6f0e260bd822069
Passion Coder <passioncoder@outlook.com>
2019-11-14 14:58:51 -0500

    Store management

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { ThemeProvider } from 'react-native-elements'
-import configureStore from './Redux/ConfigureStore'
-import RootContainer from './RootContainer'
-import theme from './Config/theme'
+import { ThemeProvider } from 'react-native-elements';
+import createStore from './Redux';
+import RootContainer from './RootContainer';
+import theme from './Config/theme';
 
-const store = configureStore()
+const store = createStore();
 
 class MainApp extends React.Component {
   render() {
     return (
       <Provider store={store}>
         <ThemeProvider theme={theme}>
-          <RootContainer/>
+          <RootContainer />
         </ThemeProvider>
       </Provider>
     );
   }
 }
 
-export default MainApp
+export default MainApp;
