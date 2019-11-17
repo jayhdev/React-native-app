3ab651fbea27bb7304835cc20b5c3173ab176954
Passion Coder <passioncoder@outlook.com>
2019-11-17 10:38:29 -0500

    Theme Color update

--- src/Config/color.js
+++ src/Config/color.js
@@ -3,9 +3,11 @@ const colors = {
   secondary: '#8430cb',
   grey0: '#aeaeae',
   grey1: '#f2f2f2',
+  grey2: '#ababab',
+  grey3: '#686868',
   background: '#fcfcfa',
   backgroundDark: '#f5f5f3',
-  black: '#060606',
+  black: '#060606'
 };
 
 export default colors;
