d65fc9ae2628845bd506366cad838a3090a5aa16
Passion Coder <passioncoder@outlook.com>
2019-11-17 20:34:37 -0500

    Add Header basic component

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,9 +1,6 @@
-import Button from './Button'
-import Input from './Input'
-import ListItem from './ListItem'
+import Button from './Button';
+import Input from './Input';
+import Header from './Header';
+import ListItem from './ListItem';
 
-export {
-  Button,
-  Input,
-  ListItem
-}
+export { Button, Input, Header, ListItem };
