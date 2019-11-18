6cbac4375d6d67d3948d8b8c9e1ddde36f6fde59
Smart Coder <smartcoder415@gmail.com>
2019-11-17 20:36:27 -0500

    Account Page for only sign out functionality

--- src/Screens/Overview/Overview.js
+++ src/Screens/Overview/Overview.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Text, View } from 'react-native';
-import Footer from '../../Layout/Footer'
+import Footer from '../../Layout/Footer';
+import Header from '../../Layout/Header';
 import styles from './styles';
 
-export default function Overview({navigation}) {
+export default function Overview({ navigation }) {
   return (
     <View style={styles.container}>
+      <Header />
       <Text>Overview Page</Text>
-      <Footer navigation={navigation}/>
+      <Footer navigation={navigation} />
     </View>
   );
 }
