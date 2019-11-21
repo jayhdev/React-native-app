c8fe0b4ee6d67e82bbc874bfa7ee06397e011ffb
Smart Coder <smartcoder415@gmail.com>
2019-11-20 22:23:04 -0500

    Theme, eslint update

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,6 +1,7 @@
+import Text from './Text';
 import Button from './Button';
 import Input from './Input';
 import Header from './Header';
 import ListItem from './ListItem';
 
-export { Button, Input, Header, ListItem };
+export { Text, Button, Input, Header, ListItem };
