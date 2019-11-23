14159f35cec0b5a8f4907d8d04b390cb1e4271c7
Passion Coder <passioncoder@outlook.com>
2019-11-22 22:29:27 -0500

    Checklist page update

--- src/Screens/Checklist/Checklist.js
+++ src/Screens/Checklist/Checklist.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { View } from 'react-native';
 import { connect } from 'react-redux';
+import { Text } from 'react-native-elements';
+
+import rootStyles from '../../styles';
 import styles from './styles';
 import Footer from '../../Layout/Footer';
 import Header from '../../Layout/Header';
@@ -17,17 +20,21 @@ class Checklist extends React.Component {
     const { checklists, navigation } = this.props;
 
     return (
-      <View>
+      <View style={rootStyles.container}>
         <Header navigation={navigation} />
-        {checklists.map(checklist => (
-          <ListItem
-            key={checklist._id}
-            title={checklist.name}
-            subtitle={checklist.name}
-            containerStyle={styles.container}
-            chevron
-          />
-        ))}
+        <View style={styles.content}>
+          <Text h1>Checklist</Text>
+          {checklists.map(checklist => (
+            <ListItem
+              key={checklist._id}
+              title={checklist.name}
+              subtitle={checklist.name}
+              subtitleStyle={styles.subtitleStyle}
+              containerStyle={styles.itemContainer}
+              chevron
+            />
+          ))}
+        </View>
         <Footer navigation={navigation} />
       </View>
     );
--- src/Screens/Checklist/styles.js
+++ src/Screens/Checklist/styles.js
@@ -2,11 +2,18 @@ import { StyleSheet } from 'react-native';
 import colors from '../../Config/color';
 
 export default StyleSheet.create({
-  container: {
-    backgroundColor: colors.backgroundDark,
+  content: {
+    flex: 1,
+    paddingLeft: 10,
+    paddingRight: 10
+  },
+  itemContainer: {
+    backgroundColor: colors.grey1,
     margin: 5,
     padding: 10,
     borderRadius: 5
   },
+  subtitleStyle: {
+    color: colors.grey3
+  }
 });
-  
