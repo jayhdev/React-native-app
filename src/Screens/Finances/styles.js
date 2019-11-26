fbfb7d061da2c6c9796da1885260e50505a190b2
Passion Coder <passioncoder@outlook.com>
2019-11-25 19:41:43 -0500

    Finance page view without image

--- src/Screens/Finances/styles.js
+++ src/Screens/Finances/styles.js
@@ -1,11 +1,37 @@
 import { StyleSheet } from 'react-native';
+import colors from '../../Config/color';
 
 export default StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  bgContainerStyle: {
+    borderWidth: 0,
+    borderRightWidth: 0,
+    borderLeftWidth: 0
   },
+  buttonStyle: {
+    backgroundColor: 'white',
+    borderWidth: 0
+  },
+  textStyle: {
+    color: colors.black
+  },
+  innerBorderStyle: {
+    width: 0
+  },
+  selectedBStyle: {
+    backgroundColor: 'white',
+    borderBottomColor: colors.secondary,
+    borderBottomWidth: 0.5
+  },
+  selectedTStyle: {
+    color: colors.secondary
+  },
+  content: {
+    flex: 1
+  },
+  leftIcon: {
+    width: 20,
+    height: 20,
+    backgroundColor: colors.grey2,
+    borderRadius: 10
+  }
 });
-  
