d0aa66ecf60ecd045636a015452730df9a3a259e
Smart Coder <smartcoder415@gmail.com>
2019-11-26 20:42:18 -0500

    Fetching EventData in the root level

--- src/Components/ListItem/index.js
+++ src/Components/ListItem/index.js
@@ -1,4 +1,11 @@
 import React from 'react';
 import { ListItem } from 'react-native-elements';
 
-export default ({ title, ...rest }) => <ListItem title={title} {...rest} />;
+export default ({ title, subTitle, rightTitle, ...rest }) => (
+  <ListItem
+    title={title}
+    subtitle={subTitle && subTitle.toString()}
+    rightTitle={rightTitle && rightTitle.toString()}
+    {...rest}
+  />
+);
