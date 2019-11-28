8bab8cd657d1df0949ccb2e14ed5e4e5772881f8
Smart Coder <smartcoder415@gmail.com>
2019-11-28 07:19:54 -0500

    Account Page View

--- src/Screens/Account/Account.js
+++ src/Screens/Account/Account.js
@@ -1,25 +1,74 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { View } from 'react-native';
-import { Button } from '../../Components';
-import * as AuthActions from '../../Redux/AuthRedux/Actions';
+import { View, Alert } from 'react-native';
+import { Text } from 'react-native-elements';
 
-class LoadingScreen extends React.Component {
-  handleSignOut = () => {
-    this.props.logout();
+import { ListItem } from '../../Components';
+import * as AuthActions from '../../Redux/AuthRedux/Actions';
+import Header from '../../Layout/Header';
+import rootStyles from '../../styles';
+import styles from './styles';
+import itemList from './Constants';
+
+const Account = props => {
+  const handleLogOut = () => {
+    Alert.alert('Sign Out', 'Are you sure you want to sign out?', [
+      {
+        text: 'Cancel',
+        style: 'cancel'
+      },
+      {
+        text: 'Confirm',
+        onPress: () => props.logout()
+      }
+    ]);
   };
 
-  render() {
-    return (
-      <View>
-        <Button title="Sign Out" onPress={this.handleSignOut} />
+  const methods = {
+    logout: handleLogOut,
+    shareLink: () => props.navigation.navigate('Share')
+  };
+
+  const handleItemPress = method => {
+    if (!methods[method]) return null;
+
+    return methods[method]();
+  };
+
+  return (
+    <View style={rootStyles.container}>
+      <Header
+        hasLeft
+        hasRight={false}
+        navigation={props.navigation}
+        title="Account"
+      />
+      <View style={styles.content}>
+        {itemList.map(item => (
+          <View key={item.name}>
+            <View style={styles.itemText}>
+              <Text style={styles.textStyle}>{item.name}</Text>
+            </View>
+            {item.lists.map(list => (
+              <ListItem
+                key={list.name}
+                title={list.name}
+                titleStyle={list.color && { color: list.color }}
+                rightElement={list.rightElement}
+                chevron={!!list.chevron}
+                bottomDivider
+                onPress={() => handleItemPress(list.method)}
+              />
+            ))}
+          </View>
+        ))}
       </View>
-    );
-  }
-}
+    </View>
+  );
+};
 
 const mapDispatchToProps = dispatch => ({
   logout: () => dispatch(AuthActions.logout())
 });
 
-export default connect(null, mapDispatchToProps)(LoadingScreen);
+export default connect(null, mapDispatchToProps)(Account);
