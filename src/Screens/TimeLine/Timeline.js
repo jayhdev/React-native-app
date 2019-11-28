5f384639dfa2997f12b3b11fa8b4e00ceda798d8
Smart Coder <smartcoder415@gmail.com>
2019-11-27 19:17:05 -0500

    TimeLine Page

--- src/Screens/TimeLine/Timeline.js
+++ src/Screens/TimeLine/Timeline.js
@@ -1,16 +1,67 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
+import { connect } from 'react-redux';
+import _ from 'lodash';
+import moment from 'moment';
 
+import { Text } from '../../Components';
 import Header from '../../Layout/Header';
 import Footer from '../../Layout/Footer';
 import rootStyles from '../../styles';
+import styles from './styles';
 
-export default function TimeLine({ navigation }) {
+function sortByDate(timelineItems) {
+  const groupedObject = _.groupBy(timelineItems, 'date');
+  const groupedArray = [];
+  _.forEach(groupedObject, (value, key) => {
+    groupedArray.push({ [key]: value });
+  });
+
+  return groupedArray;
+}
+
+function TimeLine({ navigation, timelineItems }) {
+  const sortedTimeLine = sortByDate(timelineItems);
+
   return (
     <View style={rootStyles.container}>
       <Header navigation={navigation} />
-      <Text>TimeLine Page</Text>
+      <View style={styles.content}>
+        <Text h1 style={styles.dateStyle}>
+          Timeline
+        </Text>
+        {sortedTimeLine.map(item => {
+          const dueDate = _.keys(item)[0];
+          const dayItems = item[dueDate];
+
+          return (
+            <View key={dueDate}>
+              <Text style={styles.dueDateStyle}>
+                {moment(new Date(dueDate)).format('dddd, MMMM D, YYYY')}
+              </Text>
+              <View style={styles.dateContent}>
+                {dayItems.map(time => (
+                  <View key={time._id} style={styles.timeStyle}>
+                    <Text>{time.startTime}</Text>
+                    <Text>{time.description}</Text>
+                  </View>
+                ))}
+              </View>
+            </View>
+          );
+        })}
+      </View>
       <Footer navigation={navigation} />
     </View>
   );
 }
+
+const mapStateToProps = ({
+  app: {
+    event: { timelineItems }
+  }
+}) => ({
+  timelineItems
+});
+
+export default connect(mapStateToProps)(TimeLine);
--- src/Services/api.js
+++ src/Services/api.js
@@ -23,8 +23,10 @@ export default function request(
   };
 
   if (authRequired) {
-    const store = getStore();
-    const { token } = store.getState().auth;
+    // const store = getStore();
+    // const { token } = store.getState().auth;
+    const token =
+      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZGM5MWRkOTM2ZmVmYTM2MWU1NjA4MWUiLCJlbWFpbCI6Im1vYmlsZUB0ZXN0LmNvbSIsInBhc3N3b3JkIjoiJDJhJDEwJG4wNVprWVVsanRlSHB6QkN4N3NwQ3U2MUsvNURrOS9yVEFKVTBueERFZ0lRbE83cHZJUUtXIiwiY3JlYXRlZEF0IjoiMjAxOS0xMS0xMVQwODozNzo0NS42MTJaIiwidXBkYXRlZEF0IjoiMjAxOS0xMS0xMVQwODozNzo0NS42MTJaIiwiX192IjowLCJpYXQiOjE1NzQ1OTk3MTAsImV4cCI6MTU3NTIwNDUxMH0.2dhH-vEhsELxRNBE0dwJ3SUhHUOkBN77gbxXgfKJGjc';
     requestObject.headers.Authorization = token;
   }
 
