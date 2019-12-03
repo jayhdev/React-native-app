091003f2c0782abf74bb0d40f1fe2a1287d5f2d3
Passion Coder <passioncoder@outlook.com>
2019-12-02 20:04:46 -0500

    Checklist Detail item toggling

--- src/Screens/Checklist/Detail/Detail.js
+++ src/Screens/Checklist/Detail/Detail.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View } from 'react-native';
 import { Icon } from 'react-native-elements';
+import _ from 'lodash';
 
 import rootStyles from '../../../styles';
 import { Text, ListItem } from '../../../Components';
@@ -9,42 +10,71 @@ import Header from '../../../Layout/Header';
 import styles from './styles';
 import colors from '../../../Config/color';
 
-const list = [
-  {
-    _id: '001',
-    item: 'Think about when and where you like to have your wedding',
-    checked: true
-  },
-  {
-    _id: '002',
-    item: 'Nail down a budget.',
-    checked: false
+class ChecklistDetail extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list: [
+        {
+          _id: '001',
+          item: 'Think about when and where you like to have your wedding',
+          checked: true
+        },
+        {
+          _id: '002',
+          item: 'Nail down a budget.',
+          checked: false
+        }
+      ]
+    };
   }
-];
 
-const SelectableCheckbox = checked => {
-  if (checked) {
-    return <Icon name="check-circle" color={colors.secondary} />;
+  selectableCheckbox = checked => {
+    if (checked) {
+      return <Icon name="check-circle" color={colors.secondary} />;
+    }
+
+    return <View style={styles.emptyCheckBox} />;
+  };
+
+  toggleItem = (navigation, item, index) => {
+    const { list } = this.state;
+    const tempList = _.cloneDeep(list);
+    tempList[index].checked = !tempList[index].checked;
+
+    this.setState({
+      list: tempList
+    });
+    /**
+     * @TODO Send checklistItem updated status to the backend
+     */
+  };
+
+  render() {
+    const { navigation } = this.props;
+    const { list } = this.state;
+
+    return (
+      <View style={rootStyles.container}>
+        <Header hasLeft hasRight={false} navigation={navigation} />
+        <View style={styles.content}>
+          <Text h1>12+ Months</Text>
+          {list.map((l, i) => (
+            <ListItem
+              key={l._id}
+              title={l.item}
+              bottomDivider
+              leftElement={this.selectableCheckbox(l.checked)}
+              onPress={() => this.toggleItem(navigation, l, i)}
+              containerStyle={l.checked && styles.checkedItem}
+            />
+          ))}
+        </View>
+        <Footer navigation={navigation} />
+      </View>
+    );
   }
+}
 
-  return <View style={styles.emptyCheckBox} />;
-};
-
-export default ({ navigation }) => (
-  <View style={rootStyles.container}>
-    <Header hasLeft hasRight={false} navigation={navigation} />
-    <View style={styles.content}>
-      <Text h1>12+ Months</Text>
-      {list.map((l, i) => (
-        <ListItem
-          key={l._id}
-          title={l.item}
-          bottomDivider
-          leftElement={SelectableCheckbox(l.checked)}
-          containerStyle={l.checked && styles.checkedItem}
-        />
-      ))}
-    </View>
-    <Footer navigation={navigation} />
-  </View>
-);
+export default ChecklistDetail;
