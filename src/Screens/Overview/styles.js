c3b01b3b497b6e3cb7ee35e3eeb50bc88783139e
Smart Coder <smartcoder415@gmail.com>
2019-12-02 20:05:38 -0500

    Navigation update for details page

--- src/Screens/Overview/styles.js
+++ src/Screens/Overview/styles.js
@@ -1,10 +1,8 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
-  container: {
+  content: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+    padding: 20
   }
 });
