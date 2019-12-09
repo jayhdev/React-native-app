65ebb92fa8f1a0b24a0e58743177bee1ad3cfe82
Passion Coder <passioncoder@outlook.com>
2019-12-09 11:49:58 -0500

    Import initial few query and mutations

---
+++ src/Graphql/mutations.js
@@ -0,0 +1,5 @@
+import gql from 'graphql-tag';
+
+export const SignupUserMutation = gql('./gql/mutations/signup.graphql');
+export const LoginUserMutation = gql('./gql/mutations/login.graphql');
+export const LogOutMutation = gql('./gql/mutations/logout.graphql');
