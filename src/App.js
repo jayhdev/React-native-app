fc48579ae2692272889cb918f8915b54c492f291
Passion Coder <passioncoder@outlook.com>
2019-12-09 11:49:12 -0500

    Configure apollo client

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { ThemeProvider } from 'react-native-elements';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient, InMemoryCache, HttpLink } from 'apollo-client-preset';
+
 import createStore from './Redux';
 import RootContainer from './RootContainer';
 import theme from './Config/theme';
+import envVars from '../environment';
 
 const store = createStore();
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: envVars.apiUrl
+  })
+});
 
 class MainApp extends React.Component {
   render() {
     return (
-      <Provider store={store}>
-        <ThemeProvider theme={theme}>
-          <RootContainer />
-        </ThemeProvider>
-      </Provider>
+      <ApolloProvider client={client}>
+        <Provider store={store}>
+          <ThemeProvider theme={theme}>
+            <RootContainer />
+          </ThemeProvider>
+        </Provider>
+      </ApolloProvider>
     );
   }
 }
