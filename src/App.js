65f679557b0b631c568eb7f7249907e4cc0f6883
Smart Coder <smartcoder415@gmail.com>
2019-12-11 11:36:42 -0500

    Remove Provider in the root

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 import { ThemeProvider } from 'react-native-elements';
 import { ApolloProvider } from 'react-apollo';
 import { ApolloClient, InMemoryCache, HttpLink } from 'apollo-client-preset';
 
-import createStore from './Redux';
 import RootContainer from './RootContainer';
 import theme from './Config/theme';
 import getEnvVars from '../environment';
 
 const { apiUrl } = getEnvVars();
-const store = createStore();
 const client = new ApolloClient({
   cache: new InMemoryCache(),
   link: new HttpLink({
@@ -22,11 +19,9 @@ class MainApp extends React.Component {
   render() {
     return (
       <ApolloProvider client={client}>
-        <Provider store={store}>
-          <ThemeProvider theme={theme}>
-            <RootContainer />
-          </ThemeProvider>
-        </Provider>
+        <ThemeProvider theme={theme}>
+          <RootContainer />
+        </ThemeProvider>
       </ApolloProvider>
     );
   }
