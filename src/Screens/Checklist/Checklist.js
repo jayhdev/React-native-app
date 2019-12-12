13ecee0ca6bc729ea20d29d6ba92f1081df4672e
Passion Coder <passioncoder@outlook.com>
2019-12-12 11:39:04 -0500

    Checklist Screen update

--- src/Screens/Checklist/Checklist.js
+++ src/Screens/Checklist/Checklist.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { View } from 'react-native';
-import { connect } from 'react-redux';
 import { Text } from 'react-native-elements';
 
 import rootStyles from '../../styles';
@@ -8,57 +7,34 @@ import styles from './styles';
 import Footer from '../../Layout/Footer';
 import Header from '../../Layout/Header';
 import { ListItem } from '../../Components';
-import { getChecklists } from '../../Redux/AppRedux/Actions';
 
-class Checklist extends React.Component {
-  componentDidMount() {
-    const { fetchChecklists, eventId } = this.props;
-    fetchChecklists(eventId);
-
-    this.GotoDetails = this.GotoDetails.bind(this);
-  }
-
-  static navigationOptions = {
-    title: 'Other Title'
+const Checklist = ({ navigation }) => {
+  // @TODO: Get the correct checklists with graphql
+  const checklists = [];
+  const GotoDetails = () => {
+    navigation.navigate('ChecklistDetail');
   };
 
-  GotoDetails = checklistId => {
-    this.props.navigation.navigate('ChecklistDetail');
-  };
-
-  render() {
-    const { checklists, navigation } = this.props;
-
-    return (
-      <View style={rootStyles.container}>
-        <Header navigation={navigation} />
-        <View style={styles.content}>
-          <Text h1>Checklist</Text>
-          {checklists.map(checklist => (
-            <ListItem
-              key={checklist._id}
-              title={checklist.name}
-              subtitle={checklist.name}
-              subtitleStyle={styles.subtitleStyle}
-              containerStyle={styles.itemContainer}
-              chevron
-              onPress={() => this.GotoDetails(checklist._id)}
-            />
-          ))}
-        </View>
-        <Footer navigation={navigation} />
+  return (
+    <View style={rootStyles.container}>
+      <Header navigation={navigation} />
+      <View style={styles.content}>
+        <Text h1>Checklist</Text>
+        {checklists.map(checklist => (
+          <ListItem
+            key={checklist._id}
+            title={checklist.name}
+            subtitle={checklist.name}
+            subtitleStyle={styles.subtitleStyle}
+            containerStyle={styles.itemContainer}
+            chevron
+            onPress={() => GotoDetails(checklist._id)}
+          />
+        ))}
       </View>
-    );
-  }
-}
+      <Footer navigation={navigation} />
+    </View>
+  );
+};
 
-const mapStateToProps = ({ app }) => ({
-  checklists: app.checklists,
-  eventId: app.event._id
-});
-
-const mapDispatchToProps = dispatch => ({
-  fetchChecklists: eventId => dispatch(getChecklists(eventId))
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Checklist);
+export default Checklist;
