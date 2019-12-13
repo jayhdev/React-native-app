413a13fff3ac32ef3c375052ed14f3866d0cd54c
Smart Coder <smartcoder415@gmail.com>
2019-12-13 11:44:49 -0500

    Gitignore redux folder

--- src/Redux/AppRedux/Constants.js
+++
@@ -1,5 +0,0 @@
-export const GET_EVENT = 'app/getEvent';
-export const SET_EVENT = 'app/setEvent';
-export const GET_CHECKLISTS = 'app/getchecklist';
-export const SET_CHECKLISTS = 'app/setchecklist';
-export const GET_BUDGETDETAIL = 'app/getBudgetDetail';
